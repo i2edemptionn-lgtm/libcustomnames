102a249da237bedd81a393d35f0441aeb6c2ca9f
i2edemptionn-lgtm <i2edemptionn@gmail.com>
2025-12-28 14:25:19 -0600

    Update 2025.lua
    Name Color Change for I2edemptionn

--- Console/names/2025.lua
+++ Console/names/2025.lua
@@ -5,4 +5,4 @@ local lib_name = "LibCustomNames"
 local lib = _G[lib_name]
 local n = lib.GetNamesTable()
 
-n["@GodKND"] = {"GodKND", "|cEF2B2DK|r|cEF2B2Dn|r|cFFFFFFu|r|c00205Bd|r|cFFFFFFs|r|cEF2B2De|r|cEF2B2Dn|r"}
+n["@I2edemptionn"] = {"I2edemptionn", "|c480866I|r|c421d5e2|r|c3c3155e|r|c36464dd|r|c305a44e|r|c2a6f3cm|r|c248433p|r|c1e982at|r|c18ad22i|r|c12c11ao|r|c0cd611n|r|c06ea09n|r"}
